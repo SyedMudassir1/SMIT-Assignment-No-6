eddc49051829d6329a9f9a63e3cf72cfd7b419fa
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-03-07 06:53:05 -0800

    Add files via upload

---
+++ JawaScript Assignment # 6/app.js
@@ -0,0 +1,92 @@
+
+// <====================> SMIT  Assignment # 6 <====================>
+
+//Question  # 1
+
+// var firstName = prompt("enter firstname");
+// var lastName = prompt("enter lastname")
+// var fullname = firstName + " " + lastName;
+// document.write(fullname);
+
+//Question  # 2
+
+//  var phone = "My favorite phone is: Sumsung Galaxy S6 Edge Plus";
+//  document.write(phone<br>);
+//  document.write(phone.length);
+
+//Question  # 3
+
+// var str = "pakistan";
+// document.write(str.indexOf("n"));
+ 
+//Question  # 4
+
+// var str = "Hello World";
+// document.write(str.lastIndexOf("l"));
+
+//Question  # 5
+
+// var str = "Pakistan";
+// document.write(str.charAt("3"));
+
+//Question  # 7
+
+// var city = "Hyderabad";
+// document.write(city.replace("Hyder","islam"));
+
+//Question  # 8
+
+// var msg = "Ali and Sami are best friends. They play cricket and football togethr";
+// document.write(msg.replaceAll("and","&"))
+
+//Question  # 9
+
+// var num = 472;
+// document.write(num.toString());
+
+//Question  # 10
+
+// var input = "peanuts";
+// input = input.toUpperCase();
+// document.write(input);
+
+//Question  # 11
+
+// var input = "JAWASCRIPT";
+//  input = input.toLowerCase();
+//  document.write(input);
+
+//Question  # 14
+
+// var food = prompt("WellCome to ABC Bakery.What do you want to order Sir/Ma'am");
+// food = food.toLowerCase()
+// var arr = ["cake" , "apple pie" , "cookie" , "chips" , "patties"]
+// for (var i = 0; i > arr.length; i++){
+// if(arr[i] === food){
+//  alert("cookie is available")
+//  }else{alert("We are sorry")}
+//   break;
+// }
+
+//Question  # 16
+
+// var str = "University of Karachi";
+// arr = [];
+// var word = str.split(" ");
+// console.log(word);
+
+//Question  # 17
+
+// var input = "Pakistan";
+// document.write(input.charAt(7));
+
+//Question  # 18
+
+// var str = "The quick brown fox jumps over the lazy dog";
+// document.(writestr.occurrencesOf())
+
+
+
+
+
+
